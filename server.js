ba6345532972d107490597ccd04261ac247da822
root <root@DESKTOP-OEGO3KO.localdomain>
2021-05-27 00:25:59 +0300

    a thousand issue :)

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 
 //overall creat a new application (npm init -y)
 
-const express = require ('express');//require express (npm i express)
-
 require('dotenv').config();//install dotenv(npm i dotenv)/require .env 
+
+const express = require ('express');//require express (npm i express)
 
 const weatherData =require ('./data/weather.json');//require json file
 
@@ -16,17 +16,49 @@ server.use(cors());// (allow public access permission )
 
 
 const PORT = process.env.PORT;
-
-
-server.get('/',(request,response)=>{
-console.log(`localhost:${PORT}/`);
-let dataArr = weatherData.map(item=>{
-    return item.city_name;
-})
-response.send(dataArr);
+server.get('/',(req,res) => { 
+    res.send('Test')
 })
 
 
+// http://localhost:5000/weather?cityName=Paris
+server.get('/weather',(request,response) => {
+    let cityName = request.query.cityName;
+
+console.log(query,cityName);
+
+    let resData = weatherData.find(item,idx => {if (item.city_name == cityName)        
+            return item;
+    })
+    console.log('------------------------------------',resData,'-------------------------------------');
+    try {
+        let newForcast;
+        let forcastArr=[];
+
+         forcastArr = resData.data.map (item => { 
+             data = item.valid_date;
+             description=item.weather.desqription;
+             newForcast = new Forcast(date,desqription);
+             return newForcast;
+         })
+         response.send(resData)
+    }catch(error){
+
+        response.send(`server error ${error}`)
+    }
+
+})
+
+
+class Forcast { constructor(date,desqription ){
+this.date = date
+this.desqription = desqription
+}
+}
+
+server.get('*',(request,response) => {
+response.send('Something Wrong')
+})
 
 server.listen(PORT ,()=>{
 console.log(`Server Listinig on PORT ${PORT}`);
